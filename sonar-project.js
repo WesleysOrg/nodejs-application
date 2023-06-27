29de1093e022ecf66f0d1541b7803724e3e2a7ca
Wesley Cline <118874010+WesleyCline1661@users.noreply.github.com>
2023-06-26 22:58:50 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.117.150.9:9000/',
+  serverUrl: 'http://18.224.4.255:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
